230886aee9fcc4d01e70188d6b80671194460235
Trenton Kress <trenton.kress@gmail.com>
2016-11-06 10:59:52 -0800

    Figured out the API calls in a test JS file and test HTML file.

--- public/apicalls.js
+++ public/apicalls.js
@@ -1,15 +1,4 @@
 (function(module) {
-  /*
-  <script type="text/javascript" src="//platform.linkedin.com/in.js">
-  api_key:78jbwdgk7fup6m
-  authorize:true
-  </script>
-  */
-  $.getJSON('/meetup/topics?search=' + query + '&order=members', function(data) {
-    console.log(data.results);
-  }).success(function() {
-
-  });
 
   $('#loginbutton').on('click', function(e) {
     if (!IN.User.isAuthorized()) {
@@ -17,6 +6,9 @@
         console.log('Logged in.');
         IN.API.Profile('me').fields('id,firstName,lastName,industry').result(function (data) {
           console.log(data.values[0].firstName + ' ' + data.values[0].lastName + ', ' + data.values[0].industry + ' is logged in now.');
+          $.getJSON('/meetup/topics?search=' + data.values[0].industry + '&order=members', function(data) {
+            console.log(data.results);
+          });
         });
       });
     } else {
