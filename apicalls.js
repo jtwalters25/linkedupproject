a8fa9a8db527ecf5626e8a5f1ff9c1ff1dd65ea4
Trenton Kress <trenton.kress@gmail.com>
2016-11-06 09:27:50 -0800

    Small change in my notes.

--- apicalls.js
+++ apicalls.js
@@ -1,26 +1,26 @@
 (function(module) {
   /*
-   * https://api.meetup.com/topics?key=MEETUP_KEY&search=farming&order=members
-   * https://api.meetup.com/find/groups?zip=11211&radius=1&category=34&order=members&key=MEETUP_KEY
-   */
-   /*
-      <script type="text/javascript" src="//platform.linkedin.com/in.js">
-        api_key:78jbwdgk7fup6m
-        authorize:true
-      </script>
-    */
-   $.getJSON('/meetup/topics?search=search&order=members', function(data) {
-      console.log(data.results);
-   }).success(function() {
+  * https://api.meetup.com/topics?key=MEETUP_KEY&search=farming&order=members
+  * https://api.meetup.com/find/groups?zip=11211&radius=1&category=34&order=members&key=MEETUP_KEY
+  */
+  /*
+  <script type="text/javascript" src="//platform.linkedin.com/in.js">
+  api_key:78jbwdgk7fup6m
+  authorize:true
+  </script>
+  */
+  $.getJSON('/meetup/topics?search=' + query + '&order=members', function(data) {
+    console.log(data.results);
+  }).success(function() {
 
-   });
-   
-   $('#buttontest').on('click', function(e) {
+  });
+
+  $('#loginbutton').on('click', function(e) {
     if (!IN.User.isAuthorized()) {
       IN.User.authorize(function() {
         console.log('Logged in.');
-        IN.API.Profile('me').fields('id,firstName,lastName,headline').result(function (data) {
-          console.log(data.values[0].firstName + ' ' + data.values[0].lastName + ', ' + data.values[0].headline + ' is logged in now.');
+        IN.API.Profile('me').fields('id,firstName,lastName,industry').result(function (data) {
+          console.log(data.values[0].firstName + ' ' + data.values[0].lastName + ', ' + data.values[0].industry + ' is logged in now.');
         });
       });
     } else {
